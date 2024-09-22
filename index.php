00580918063f8ee556651c4997fbe269366fc0f9
kevinpermana212 <asusk21@gmail.com>
2024-09-22 11:14:50 +0700

    pembuatan index profil saya menggunakan bahasa php

--- index.php
+++ index.php
@@ -3,10 +3,71 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Selamat Datang Home</title>
 </head>
 <body>
-    <h1>Selamat datang di website profile saya</h1>
+    <nav>
+        <a href="Index.php">Beranda</a>
+        <a href="cv.pdf">Download cv saya</a>
+        <a href="contact.php">Kontak</a>
+        <a href="tentang.php">Tentang Saya</a>
+    </nav>
+    
+    <hr />
+    <header style="text-align: center;">
+        <img src="assets/logo.jpeg" width="200" height="200" style="border-radius :50%;" />
+        <h1>Kairos 62</h1>
+        <p>Web Developer</p>
+    </header>
+    <hr/>
+
+    <article style="text-align: center;">
+        <h2>
+            Overview
+        </h2>
+        <p>
+        Hai, saya seorang programmer yang sedang mendalami HTML dan CSS. Senang bisa berkenalan!
+        </p>
+    </article>
+
+    <div style="max-width: 600px; margin: 3em auto">
+        <table border="1" width:"100%">
+            <thead>
+                <tr>
+                    <th>
+                        Skill
+                    </th>
+                    <th>
+                        Pengalaman
+                    </th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>
+                        <ul>
+                            <li>Problem Solving</li>
+                            <li>Java Script (Beginer)</li>
+                            <li>CSS (Expert) </li>
+                        </ul>
+                    </td>
+                    <td>
+                        <ul>
+                            <li>Pengembangan AI XXX</li>
+                            <li>Data Analis</li>
+                            <li>Portofolio web Perusahaan</li>
+                        </ul>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+
+    <hr>
+    <footer style="text-align: center;">
+        <p>Hak Cipta © 2024 kairos62. Semua hak dilindungi undang-undang.</p>
+    </footer>
+
     
 </body>
 </html>
