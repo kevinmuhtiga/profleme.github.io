aa27b23f73f3346d28a70ef225f1907433b97f16
kevinpermana212 <asusk21@gmail.com>
2024-09-22 15:53:13 +0700

    penambahan tentang php

---
+++ tentang.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Selamat Datang Home</title>
+</head>
+<body>
+    <nav>
+        <a href="Index.php">Beranda</a>
+        <a href="cv.pdf">Download cv saya</a>
+        <a href="contact.php">Kontak</a>
+        <a href="tentang.php">Tentang Saya</a>
+    </nav>
+    
+    <hr />
+    <h1>About Me</h1>
+    <pre>
+    <b>Saya, Kairos62,</b> seorang pengembang web pemula yang antusias dengan dunia coding. 
+    Saya sedang dalam proses belajar HTML dan CSS untuk menciptakan desain web yang 
+    menarik dan responsif. Melalui proyek-proyek kecil, saya terus mengasah kemampuan 
+    dalam membangun struktur halaman web yang baik dan menerapkan gaya visual yang sesuai.
+    </pre>
+
+    <br>
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/KdlsQHNIi0Y?si=32T0xu2OpPouxHjG" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+
+    <hr>
+    <footer style="text-align: center;">
+        <p>Hak Cipta © 2024 kairos62. Semua hak dilindungi undang-undang.</p>
+    </footer>
+
+    
+</body>
+</html>
